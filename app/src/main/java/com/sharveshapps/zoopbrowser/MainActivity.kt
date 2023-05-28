7051f279e5b37c4102580edd79f4cf7e22c9b6f8
sharvesh <sharveshsreethar0806@gmail.com>
2023-05-28 19:27:14 +0530

    ViewPager2 with HomeFragment implemented

--- app/src/main/java/com/sharveshapps/zoopbrowser/MainActivity.kt
+++ app/src/main/java/com/sharveshapps/zoopbrowser/MainActivity.kt
@@ -2,14 +2,29 @@ package com.sharveshapps.zoopbrowser
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.sharveshapps.zoopbrowser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+
+    companion object {
+    var tabsList: ArrayList<Fragment> = ArrayList()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        tabsList.add(HomeFragment())
+        binding.myPager.adapter = TabsAdapter(supportFragmentManager, lifecycle)
+    }
+    private inner class TabsAdapter(fa: FragmentManager, lc: Lifecycle) : FragmentStateAdapter(fa,lc) {
+        override fun getItemCount(): Int = tabsList.size
+
+        override fun createFragment(position: Int): Fragment = tabsList[position]
     }
 }
