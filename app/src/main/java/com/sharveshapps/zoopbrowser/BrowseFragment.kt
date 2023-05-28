5c6a5633decda0b24ba689d8b9775d7bc6bb6041
sharvesh <sharveshsreethar0806@gmail.com>
2023-05-28 20:00:57 +0530

    Custom webViewClient & WebChromeClient set and js enabled

--- app/src/main/java/com/sharveshapps/zoopbrowser/BrowseFragment.kt
+++ app/src/main/java/com/sharveshapps/zoopbrowser/BrowseFragment.kt
@@ -1,15 +1,19 @@
 package com.sharveshapps.zoopbrowser
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.sharveshapps.zoopbrowser.databinding.FragmentBrowseBinding
 
 class BrowseFragment : Fragment() {
 
     private lateinit var binding: FragmentBrowseBinding
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,7 +21,19 @@ class BrowseFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_browse, container, false)
         binding = FragmentBrowseBinding.bind(view)
-        binding.webView.loadUrl("https://www.google.com")
+        binding.webView.apply {
+            settings.javaScriptEnabled=true
+            settings.setSupportZoom(true)
+            settings.builtInZoomControls=true
+            settings.displayZoomControls=false
+            webViewClient= WebViewClient()
+            webChromeClient= WebChromeClient()
+
+            loadUrl("https://www.google.com")
+
+
+        }
+
         return view
     }
 
